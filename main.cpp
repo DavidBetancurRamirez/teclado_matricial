02fb9ceb8677b829a83e58f61c30458671c1b454
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-08-31 20:08:38 +0000

    First commit

---
+++ main.cpp
@@ -0,0 +1,53 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include <iostream>
+#include <string>
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     200ms
+
+// Objeto para establecer la comunicación serial con el Pc
+UnbufferedSerial serial(USBTX, USBRX, 9600);
+
+// Objeto pa leer la matriz numerica
+DigitalIn filas[] = {D2, D3, D4, D5};
+DigitalOut columnas[] = {D6, D7, D8};
+
+using namespace std;
+
+string teclado[][3] = {
+    {"1", "2", "3"},
+    {"4", "5", "6"},
+    {"7", "8", "9"},
+    {"*", "0", "#"}
+};
+
+string escribir() {
+    for (int i=0; i<3; i++) {
+        columnas[i] = 0;
+
+        for (int j=0; j<4; j++) {
+            if (filas[j]==0) {
+                columnas[i] = 1;
+                return teclado[j][i];
+            }
+        }
+
+        columnas[i] = 1;
+    }
+
+    return "";
+}
+
+int main()
+{
+    while (true) {
+        string tecla = escribir();
+        if (tecla!="") cout << tecla << endl;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
